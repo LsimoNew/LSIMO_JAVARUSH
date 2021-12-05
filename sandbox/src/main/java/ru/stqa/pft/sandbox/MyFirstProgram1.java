7dc00cf09fa144a1e84425a4be04f6b11ff691d4
LsimoNew <lsimo@mail.ru>
2021-12-05 15:45:22 +0300

    Изменение форматирования текста

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
@@ -2,6 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram1 {
 
-	public static void main(String[] args) { System.out.println("Hello, world!"); }
-	
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+
 }
